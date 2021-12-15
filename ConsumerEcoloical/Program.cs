9cf159660815f30048772acfe3d62283550a7981
o.burkovskyi <o.burkovskyi@temabit.com>
2021-12-15 10:20:38 +0200

    Добавьте файлы проекта.

---
+++ ConsumerEcoloical/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ConsumerEcoloical
+{
+    class Program
+    {
+        private static readonly string exchangeName = "topic_logs";
+
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: exchangeName, type: ExchangeType.Topic);
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                channel.QueueBind(queue: queueName,
+                                  exchange: exchangeName,
+                                  routingKey: "*.*.*.ecological");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body.ToArray());
+                    Console.WriteLine("Received message: {0}", message);
+                };
+
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine($"Subscribed to the queue {queueName}");
+                Console.WriteLine("Listerning to [*.*.*.ecological]");
+                Console.ReadKey();
+            }
+        }
+    }
+}
